e569ce21fa24e9233ae672b19f246a3c53977ae2
nbabiera <nbabiera@newbrighton.edu.ph>
2024-08-16 19:06:22 +0800

    listing of data

--- src/components/Catering.jsx
+++ src/components/Catering.jsx
@@ -4,7 +4,8 @@ import calderata from "../resources/images/Caldereta.jpg";
 import friedchicken from "../resources/images/friend-chicken.jpg";
 import mangofloat from "../resources/images/mango-float.jpg";
 import shanghai from "../resources/images/shanghai.jpg";
-import dishdetails from "../dishdetails.json";
+import dishes from "../resources/dishes.json";
+import jobs from "../jobs.json"
 
 const Catering = () => {
   const imgList = [beefsteak, calderata, friedchicken, mangofloat, shanghai];
@@ -12,11 +13,11 @@ const Catering = () => {
     <div className="my-24">
       <h1 className="text-4xl my-4">Plan your next event with us!</h1>
       <div className="flex flex-wrap justify-center items-center">
-        {dishdetails.map((dish) => (
-          <div className="bg-white-500 shadow-xl rounded overflow-hidden w-80">
-            <img src={beefsteak} alt="Beef Steak" className="w-80 h-64" />
-            <h1 className="text-xl bold">{dish.title}</h1>
-            <p className="mx-2">{dish.desc}</p>
+        {jobs.map((job) => (
+          <div className="bg-white-500 shadow-xl rounded overflow-hidden w-80 my-2 mx-2">
+            {/* <img src={img} alt="Beef Steak" className="w-80 h-64 "/> */}
+            <h1 className="text-xl bold">{job.title}</h1>
+            <p className="mx-2">{job.description}</p>
           </div>
         ))}
       </div>
