3a184250f87e0b4814baa248b9f10ecdedefb849
nbabiera <nbabiera@newbrighton.edu.ph>
2024-08-16 18:32:16 +0800

    list dish details

--- src/components/Catering.jsx
+++ src/components/Catering.jsx
@@ -4,24 +4,21 @@ import calderata from "../resources/images/Caldereta.jpg";
 import friedchicken from "../resources/images/friend-chicken.jpg";
 import mangofloat from "../resources/images/mango-float.jpg";
 import shanghai from "../resources/images/shanghai.jpg";
+import dishdetails from "../dishdetails.json";
 
 const Catering = () => {
-    const imgList = [beefsteak,calderata,friedchicken,mangofloat,shanghai]
+  const imgList = [beefsteak, calderata, friedchicken, mangofloat, shanghai];
   return (
     <div className="my-24">
       <h1 className="text-4xl my-4">Plan your next event with us!</h1>
       <div className="flex flex-wrap justify-center items-center">
-        <div className="bg-white-500 shadow-xl rounded overflow-hidden w-80">
-          <img src={beefsteak} alt="Beef Steak" className="w-80 h-64" />
-          <h1 className="text-xl bold">Beef Steak</h1>
-          <p className="mx-2">
-            {" "}
-            Beef steak dish also known as "Bistek Tagalog" that features tender
-            slices of beef cooked in a rich sauce made from soy sauce, calamansi
-            (or lemon), onions, and sometimes garlic. It's typically served with
-            rice and is often garnished with fried onions.
-          </p>
-        </div>
+        {dishdetails.map((dish) => (
+          <div className="bg-white-500 shadow-xl rounded overflow-hidden w-80">
+            <img src={beefsteak} alt="Beef Steak" className="w-80 h-64" />
+            <h1 className="text-xl bold">{dish.title}</h1>
+            <p className="mx-2">{dish.desc}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
